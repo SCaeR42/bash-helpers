87611e73db7978edd2e02b219ef10b069ca68876
CaSPeR <16041701+Casperfl@users.noreply.github.com>
2021-04-04 02:29:36 +0000

    change default local aliases

--- local/local_aliases.sh
+++ local/local_aliases.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # Custom aliases for server
 
-alias gpush='git push'
-alias dev='cd devSitePath'
-alias prod='cd prodSitePath'
+#alias gpush='git push'
+#alias dev='cd devSitePath'
+alias prod='cd /var/www/html/'
 
 alias bh='echo "
-gpush 	- 	git push
-dev 	- 	cd devSitePath
-prod 	- 	cd prodSitePath
+prod 	- 	cd /var/www/html/
 "'
